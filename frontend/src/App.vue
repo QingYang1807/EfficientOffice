952c6fb572d15e7b36b33853c1fe1340e5896556
LinQingYang <15274452497@163.com>
2023-05-08 17:40:08 +0800

    update App.Vue and main.js

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,10 @@
 <template>
-  <HelloWorld/>
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: { 
-    HelloWorld
-  }
 }
 </script>
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.min.css'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
